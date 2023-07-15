430e819a8357c4f274747f7f032cf305362b9a0b
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-07-15 15:27:20 +0300

    add project assets

---
+++ app/src/main/java/com/mr_alhams/gonuts/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.mr_alhams.gonuts.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.mr_alhams.gonuts.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular, weight = FontWeight.Normal),
+    Font(R.font.inter_medium, weight = FontWeight.Medium),
+    Font(R.font.inter_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.inter_bold, weight = FontWeight.Bold),
+)
---
+++ app/src/main/java/com/mr_alhams/gonuts/ui/theme/Style.kt
@@ -0,0 +1,82 @@
+package com.mr_alhams.gonuts.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val titleSmall = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    color = Black
+)
+
+val titleMedium = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 18.sp,
+    color = Black
+)
+
+val titleSmallMedium = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    color = Black
+)
+
+val titleLargeSemibold = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 20.sp,
+    color = Black
+)
+
+val titleXLarge = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 22.sp,
+    color = Black
+)
+
+val headlineSmallSemibold = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 30.sp,
+    color = Black
+)
+
+val headlineLargeSemibold = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 54.sp,
+    color = Primary
+)
+
+val bodySmallNormal = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    color = Gray
+)
+
+val bodyMediumSemibold = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 14.sp,
+    color = Gray
+)
+
+val bodyMedium = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    color = Gray
+)
+
+val bodyMediumNormal = TextStyle(
+    fontFamily = Inter,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    color = Gray
+)
